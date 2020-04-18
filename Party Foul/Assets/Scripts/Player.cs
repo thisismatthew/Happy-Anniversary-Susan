9134848efceb793d99321c6ea7416688757a97a0
Matthew Jackson <102381833@student.swin.edu.au>
2020-04-18 13:59:28 +1000

    -  player controller -  art added for background and idle player -  rigidbody + collider added for player -  polygon colliders added for background walls

---
+++ Party Foul/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float move_speed = 5f;
+    Vector2 movement;
+    // Start is called before the first frame update
+
+  
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+	{
+        rb.MovePosition(rb.position + movement * move_speed * Time.fixedDeltaTime);
+	}
+}
